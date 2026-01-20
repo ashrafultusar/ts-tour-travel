2415152ff9f94df80b20aeae5f6ce79ddc92d733
ashrafultusar <ashrafulislamtusar@gmail.com>
2026-01-21 00:46:51 +0600

    added our university code

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/home/heroSection/HeroSection";
+import OurUniversities from "@/components/home/ourUniversities/OurUniversities";
 
 export default function Home() {
   return (
     <div>
-   <HeroSection></HeroSection>
+   <HeroSection/>
+   <OurUniversities/>
     </div>
   );
 }
