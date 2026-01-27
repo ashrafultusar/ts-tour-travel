adf0b05b2d24ff9872ec69e45e244c30f4f86ade
Mohammad Atikuzzaman <akash203037@gmail.com>
2026-01-27 23:54:21 +0600

    production config added

--- next.config.ts
+++ next.config.ts
@@ -1,58 +1,59 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "images.unsplash.com",
-                pathname: "**",
-            },
-            {
-                protocol: "https",
-                hostname: "res.cloudinary.com",
-                pathname: "**",
-            },
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "2mb",
+    },
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
         ],
-    },
-    experimental: {
-        serverActions: {
-            bodySizeLimit: "2mb",
-        },
-    },
-    async headers() {
-        return [
-            {
-                source: "/:path*",
-                headers: [
-                    {
-                        key: "X-DNS-Prefetch-Control",
-                        value: "on",
-                    },
-                    {
-                        key: "Strict-Transport-Security",
-                        value: "max-age=63072000; includeSubDomains; preload",
-                    },
-                    {
-                        key: "X-Content-Type-Options",
-                        value: "nosniff",
-                    },
-                    {
-                        key: "X-Frame-Options",
-                        value: "SAMEORIGIN",
-                    },
-                    {
-                        key: "X-XSS-Protection",
-                        value: "1; mode=block",
-                    },
-                    {
-                        key: "Referrer-Policy",
-                        value: "origin-when-cross-origin",
-                    },
-                ],
-            },
-        ];
-    },
+      },
+    ];
+  },
 };
 
 export default nextConfig;
